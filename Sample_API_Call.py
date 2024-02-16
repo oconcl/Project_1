bab464ee99136102e96f8d09d9148dc0851333b6
stevezg <anderssj@usc.edu>
2024-02-15 20:31:55 -0800

    Add CSV and JSON file creation to Sample_API_Call.py

--- Sample_API_Call.py
+++ Sample_API_Call.py
@@ -1,8 +1,21 @@
 import requests
+import csv
+import json
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&outputsize=full&apikey=demo'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&outputsize=full&apikey=NMA67JBGVAXFK6SH'
 r = requests.get(url)
 data = r.json()
 
-print(data)
+# Create a CSV file called "results.csv"
+with open('results.csv', 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(data.keys())
+    writer.writerows(data.values())
+
+print("CSV file created: results.csv")
+
+# Create a JSON file called "results.json"
+with open('results.json', 'w') as json_file:
+    json.dump(data, json_file)
+
+print("JSON file created: results.json")
